2fc6d1abc5cc42730dbb9df5766817009d19adca
pearl <kjj0837@naver.com>
2024-10-01 15:10:35 +0900

    기초

---
+++ src/main/java/com/mysite/mybatisdb/MybatisdbApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.mybatisdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mysite/mybatisdb/MybatisdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.mybatisdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
